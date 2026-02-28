c7999e9ebc1737bb90799e6a5dbaf5412ebc6853
Thanapong Angkha <lindstorm76@outlook.com>
2026-02-28 16:07:34 +0700

    add support for browsers

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), legacy()],
   test: {
     globals: true,
     environment: "jsdom",
